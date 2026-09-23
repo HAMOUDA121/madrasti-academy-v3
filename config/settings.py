6590525ab94e1913b0d209721fafb347f4d33dc7
Hamouda <tarchihamouda48@gmail.com>
2026-09-23 12:38:21 +0200

    Fix STATIC_ROOT and WhiteNoise settings for production

--- config/settings.py
+++ config/settings.py
@@ -61,3 +61,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Production Static Files Configuration
+import os
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
